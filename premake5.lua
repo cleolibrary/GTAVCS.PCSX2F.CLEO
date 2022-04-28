842f7fba46ce9e93978ff647cba12cc56c02bc9e
ThirteenAG <thirteenag@gmail.com>
2022-04-28 14:51:34 +0300

    init

--- premake5.lua
+++ premake5.lua
@@ -1,46 +1,39 @@
-workspace "SOLUTION_NAME"
+workspace "GTAVCS.PCSX2F.CLEO"
    configurations { "Release", "Debug" }
-   platforms { "Win32" }
-   architecture "x32"
+   platforms { "Win64" }
+   architecture "x64"
    location "build"
    objdir ("build/obj")
    buildlog ("build/log/%{prj.name}.log")
-   buildoptions {"-std:c++latest"}
+   cppdialect "C++latest"
+   include "makefile.lua"
    
    kind "SharedLib"
    language "C++"
-   targetdir "data/%{prj.name}/scripts"
+   targetdir "data/scripts"
    targetextension ".asi"
    characterset ("UNICODE")
    staticruntime "On"
    
-   defines { "rsc_CompanyName=\"CompanyName\"" }
+   defines { "rsc_CompanyName=\"ThirteenAG\"" }
    defines { "rsc_LegalCopyright=\"MIT License\""} 
    defines { "rsc_FileVersion=\"1.0.0.0\"", "rsc_ProductVersion=\"1.0.0.0\"" }
    defines { "rsc_InternalName=\"%{prj.name}\"", "rsc_ProductName=\"%{prj.name}\"", "rsc_OriginalFilename=\"%{prj.name}.asi\"" }
-   defines { "rsc_FileDescription=\"FileDescription\"" }
-   defines { "rsc_UpdateUrl=\"UpdateUrl\"" }
+   defines { "rsc_FileDescription=\"PCSX2 Plugin Injector\"" }
+   defines { "rsc_UpdateUrl=\"https://github.com/ThirteenAG/GTAVCS.PCSX2F.CLEO\"" }
    
    files { "source/%{prj.name}/*.cpp" }
    files { "Resources/*.rc" }
    files { "external/hooking/Hooking.Patterns.h", "external/hooking/Hooking.Patterns.cpp" }
    files { "includes/stdafx.h", "includes/stdafx.cpp" }
    includedirs { "includes" }
+   includedirs { "source/api" }
    includedirs { "external/hooking" }
    includedirs { "external/injector/include" }
    includedirs { "external/inireader" }
    includedirs { "external/spdlog/include" }
    includedirs { "external/filewatch" }
    includedirs { "external/modutils" }
-   
-   --local dxsdk = os.getenv "DXSDK_DIR"
-   --if dxsdk then
-   --   includedirs { dxsdk .. "/include" }
-   --   libdirs { dxsdk .. "/lib/x86" }
-   --else
-   --   includedirs { "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/include" }
-   --   libdirs { "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/lib/x86" }
-   --end
    
    pbcommands = { 
       "setlocal EnableDelayedExpansion",
@@ -50,7 +43,7 @@ workspace "SOLUTION_NAME"
       "set filename=%%~ni",
       "set fileextension=%%~xi",
       "set target=!path!!filename!!fileextension!",
-      "if exist \"!target!\" copy /y \"!file!\" \"!target!\"",
+      "if exist \"!target!\" copy /y \"%%~fi\" \"!target!\"",
       ")" }
 
    function setpaths(gamepath, exepath, scriptspath)
@@ -66,13 +59,31 @@ workspace "SOLUTION_NAME"
             debugdir (gamepath .. (dir or ""))
          end
       end
-      targetdir ("data/%{prj.name}/" .. scriptspath)
+      targetdir ("data/" .. scriptspath)
    end
    
-   function add_asmjit()
-      files { "external/asmjit/src/**.cpp" }
-      includedirs { "external/asmjit/src" }
+   function setbuildpaths_ps2(gamepath, exepath, scriptspath, ps2sdkpath, sourcepath, prj_name)
+      local pbcmd = {}
+      for k,v in pairs(pbcommands) do
+        pbcmd[k] = v
+      end
+      if (gamepath) then
+         cmdcopy = { "set \"path=" .. gamepath .. scriptspath .. "\"" }
+         pbcmd[2] = "set \"file=../data/" .. prj_name .. "/" .. scriptspath .. prj_name ..".elf\""
+         table.insert(cmdcopy, pbcmd)
+         buildcommands   { "call " .. ps2sdkpath .. " -C " .. sourcepath, cmdcopy }
+         rebuildcommands { "call " .. ps2sdkpath .. " -C " .. sourcepath .. " clean && " .. ps2sdkpath .. " -C " .. sourcepath, cmdcopy }
+         cleancommands   { "call " .. ps2sdkpath .. " -C " .. sourcepath .. " clean" }
+         debugdir (gamepath)
+         if (exepath) then
+            debugcommand (gamepath .. exepath)
+            dir, file = exepath:match'(.*/)(.*)'
+            debugdir (gamepath .. (dir or ""))
+         end
+      end
+      targetdir ("data/" .. scriptspath)
    end
+
       
    filter "configurations:Debug*"
       defines "DEBUG"
@@ -83,11 +94,14 @@ workspace "SOLUTION_NAME"
       optimize "On"
 
 
-project "x86"
-   setpaths("", "")
-project "x64"
-   platforms { "Win64" }
-   architecture "x64"
-   add_asmjit()
-   setpaths("", "")
-
+project "GTAVCS.PCSX2F.CLEO"
+   kind "Makefile"
+   includedirs { "external/ps2sdk/ps2sdk/ee" }
+   files { "source/%{prj.name}/*.h" }
+   files { "source/%{prj.name}/*.c" }
+   targetextension ".elf"
+   setbuildpaths_ps2("Z:/GitHub/PCSX2-Fork-With-Plugins/bin/", "pcsx2x64.exe", "PLUGINS/", "%{wks.location}/../external/ps2sdk/ee/bin/vsmake", "%{wks.location}/../source/%{prj.name}/", "GTAVCS.PCSX2F.CLEO")
+   writemakefile_ps2("GTAVCS.PCSX2F.CLEO", "PLUGINS/", "0x05000000", "../../includes/pcsx2/log.o",
+   "../../includes/pcsx2/memalloc.o", "../../includes/pcsx2/patterns.o", "../../includes/pcsx2/injector.o", "../../includes/pcsx2/rini.o",
+   "../../includes/pcsx2/inireader.o", "../../includes/pcsx2/mips.o")
+   writelinkfile_ps2("GTAVCS.PCSX2F.CLEO")
